9a8b7b8ebd5c5a01a2ca4fe7dc8452f4ea483a0e
wassertim <tim.tkachenko@idealo.de>
2021-11-24 20:20:34 +0100

    remove index assignment

--- src/db.ts
+++ src/db.ts
@@ -9,7 +9,7 @@ const uri = `mongodb://${process.env.MONGO_HOST}:27017/quiz`;
 export const mongoConnect = async () => {
     const client = await MongoClient.connect(uri);
     _db = client.db("quiz");
-    await _db.collection("users").createIndex({ login: 1 }, { unique: true });
+    // await _db.collection("users").createIndex({ login: 1 }, { unique: true });
     await _db.collection("quizzes").createIndex({ createdBy: 1 }, { unique: false });
 
     return { _db, client };
